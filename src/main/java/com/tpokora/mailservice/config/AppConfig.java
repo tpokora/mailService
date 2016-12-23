e205c1802e7a23abbf091e58711dcbb564401e13
tpokora <pokora.tomek@gmail.com>
2016-12-23 22:15:01 +0100

    CORS config

--- src/main/java/com/tpokora/mailservice/config/AppConfig.java
+++ src/main/java/com/tpokora/mailservice/config/AppConfig.java
@@ -4,6 +4,9 @@ import com.tpokora.mailservice.email.sender.EmailHtmlSender;
 import com.tpokora.mailservice.email.sender.EmailSender;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Created by pokor on 17.12.2016.
@@ -19,5 +22,18 @@ public class AppConfig {
     @Bean(name = "emailHtmlSender")
     public EmailHtmlSender getEmailHtmlSender() {
         return new EmailHtmlSender();
+    }
+
+    // CORS
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+                // opening DEV application to request different then front end application - must be reconfigured in production system
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
     }
 }
