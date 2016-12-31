5b83f38e5d651992ec0bd8a1cd9c6394717f4425
tpokora <pokora.tomek@gmail.com>
2016-12-31 15:17:52 +0100

    Different property file for tests

--- src/test/java/com/tpokora/mailservice/common/AbstractTest.java
+++ src/test/java/com/tpokora/mailservice/common/AbstractTest.java
@@ -6,6 +6,7 @@ import com.tpokora.mailservice.email.sender.EmailSender;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
@@ -16,5 +17,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
         DatabaseTestConfiguration.class
 })
 @RunWith(SpringJUnit4ClassRunner.class)
+@TestPropertySource("classpath:properties/dev.properties")
 public class AbstractTest {
 }
--- src/test/java/com/tpokora/mailservice/config/DatabaseTestConfiguration.java
+++ src/test/java/com/tpokora/mailservice/config/DatabaseTestConfiguration.java
@@ -1,12 +1,10 @@
 package com.tpokora.mailservice.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 /**
  * Created by pokor on 24.12.2016.
  */
 @Configuration
-@PropertySource("classpath:properties/dev.properties")
 public class DatabaseTestConfiguration extends DatabaseConfiguration {
 }
